726ec999db358b0daadfc9702e9345ce737226cd
Anggie Alava <131820456+AngelesGit@users.noreply.github.com>
2023-07-08 04:24:32 +0000

    Hoy implemente lo que aprendi de interpolarizacion de codigo

--- src/app.js
+++ src/app.js
@@ -12,28 +12,21 @@ window.onload = () => {
 
   console.log("Hello Rigo from the console!");
 };
-let generateExcuse = () => {
-  let pronoun = ["A", "The"];
-  let subject = ["jogger", "racoon", "dog", "driver", "comedian", "pincone"];
-  let action = ["took my", "threw my", "yelled at my", "stole my", "bit my"];
-  let possetion = ["homework", "toe", "car", "shoe"];
-  let where = ["on the street", "in my house", "in my driveway"];
 
-  let proIndx = Math.floor(Math.random() * pronoun.length);
-  let subIndx = Math.floor(Math.random() * subject.length);
-  let actionIndex = Math.floor(Math.random() * action.length);
-  let possetionIndex = Math.floor(Math.random() * possetion.length);
-  let whereIndex = Math.floor(Math.random() * where.length);
+const pronouns = ["A", "The"];
+const subjects = ["jogger", "racoon", "dog", "driver", "comedian", "pincone"];
+const actions = ["took my", "threw my", "yelled at my", "stole my", "bit my"];
+const items = ["homework", "toe", "car", "shoe"];
+const places = ["on the street", "in my house", "in my driveway"];
 
-  return (
-    pronoun[proIndx] +
-    " " +
-    subject[subIndx] +
-    " " +
-    action[actionIndex] +
-    " " +
-    possetion[possetionIndex] +
-    " " +
-    where[whereIndex]
-  );
-};
+function randompick(word_list) {
+  return word_list[Math.floor(Math.random() * word_list.length)];
+}
+
+function generateExcuse() {
+  return `${randompick(pronouns)} ${randompick(subjects)} ${randompick(
+    actions
+  )} ${randompick(items)} ${randompick(places)}.`;
+}
+
+//
